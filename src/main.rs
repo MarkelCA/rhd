c8f2a042ee1a71d3aa556abdc20f7384bec4c345
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-05-10 19:48:57 +0200

    Lazy loading the dump result

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         .context(format!("Reading file {}",args.file_path))
         .expect("Provided file not found");
 
-    for line in rhd::dumpv2(f) {
+    for line in rhd::dump(f) {
         println!("{}",line);
     }
 }
