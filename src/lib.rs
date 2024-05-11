d1d07cfb6ce75944b8e519bc38bf44469278b905
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-05-11 17:38:23 +0200

    fixed byte offset + optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-use std::io::{BufReader,Read};
+use std::io::{BufReader,Read, Seek};
 
 const CHUNK_SIZE: usize = 16;
 
 pub fn dump(f: std::fs::File) -> impl Iterator<Item = String>  {
     let mut b = BufReader::with_capacity(CHUNK_SIZE, f);
-    let mut line = 0;
 
     std::iter::from_fn(move || {
         let buffer = &mut [0;CHUNK_SIZE];
+        let line_offset = b.stream_position().expect("Failed getting the stream position");
         let bytes_read = b.read(buffer).expect("Failed reading the byte buffer");
 
         if bytes_read == 0 {
@@ -20,26 +20,24 @@ pub fn dump(f: std::fs::File) -> impl Iterator<Item = String>  {
         let line_bytes = buffer[..bytes_read].to_vec();
 
         for chunk in line_bytes.chunks(2) {
-            let first_byte  = chunk.get(0).unwrap();
-            let second_byte = chunk.get(1) ;
+            let first_byte  = chunk.get(0).unwrap(); // We should always have the first byte
+            let second_byte = chunk.get(1);
 
-            let mut byte_pair_str = String::new();
+            let mut byte_pair_str = String::with_capacity(2);
 
             byte_pair_str.push_str(&format!("{:0>2x}",first_byte));
             ascii_line.push(get_printable(*first_byte));
 
-            // If the file is odd the second byte will be None
+            // If the file is odd the second byte will be None at the last iteration
             if let Some(byte) = second_byte {
-                let second_byte_str = format!("{:x}",byte);
-                byte_pair_str.push_str(&format!("{:0>2} ",second_byte_str));
+                byte_pair_str.push_str(&format!("{:0>2x} ",byte));
                 ascii_line.push(get_printable(*byte));
 
             }
             hex_line.push_str(format!("{}",byte_pair_str).as_str());
         }
 
-        let line_str = format!("{:0>8x}: {:<40} | {}", line*CHUNK_SIZE, hex_line,ascii_line);
-        line+=1;
+        let line_str = format!("{:0>8x}: {:<40}  {}", line_offset, hex_line,ascii_line);
 
         Some(line_str)
     })
@@ -47,7 +45,7 @@ pub fn dump(f: std::fs::File) -> impl Iterator<Item = String>  {
 
 fn get_printable(byte: u8) -> char {
     match byte {
-        32..=127 => char::from_u32(byte as u32).unwrap(),
+        32..=127 => char::from(byte),
         _ => '.'
     }
 }
